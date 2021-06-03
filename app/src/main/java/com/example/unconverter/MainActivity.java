31884d9a60d2521e1121a02d25a1c6c7ff395bf9
Keshav <kumarkeshav825@gmail.com>
2021-06-04 01:45:41 +0530

    add app

--- app/src/main/java/com/example/unconverter/MainActivity.java
+++ app/src/main/java/com/example/unconverter/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
   public class MainActivity extends AppCompatActivity {
 private Button button;
 private TextView textview;
@@ -22,35 +24,50 @@ private ImageView imageView;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button =findViewById(R.id.button);
-        editText =findViewById(R.id.editTextText);
-        textview=findViewById(R.id.textView3);
-        imageView=findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editTextText);
+        textview = findViewById(R.id.textView3);
+        imageView = findViewById(R.id.imageView);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//
+//
+//
+//                    String str = editText.getText().toString();
+//                    int kg = Integer.parseInt(str);
+//                if(kg==0 || kg<0)
+//                    Toast.makeText(MainActivity.this, "Enter Correct Input", Toast.LENGTH_SHORT).show();
+//
+//
+//                    double pound = kg * 2.20462d;
+//                    String pattern = "#.##";
+//                DecimalFormat  decimalFormat = new DecimalFormat(pattern);
+//                        String formattDouble =decimalFormat.format(pound);
+//                    textview.setText("The value of pound is : " +formattDouble);
+//
+//
+//
+//
+//
+//            }
+//        });
+    }
 
 
+        public void calculate(View view){
+            String str = editText.getText().toString();
+                  int kg = Integer.parseInt(str);
+            double pound = kg * 2.20462d;
+                    String pattern = "#.##";
+                DecimalFormat  decimalFormat = new DecimalFormat(pattern);
+                        String formattDouble =decimalFormat.format(pound);
+                    textview.setText("The value of pound is : " +formattDouble);
 
-
-                    String str = editText.getText().toString();
-                    int kg = Integer.parseInt(str);
-                if(kg==0 || kg<0)
-                    Toast.makeText(MainActivity.this, "Enter Correct Input", Toast.LENGTH_SHORT).show();
-
-
-                    double pound = kg * 2.20462;
-                    textview.setText("The value is " +pound);
-
-
-
-
-
-            }
-        });
+        }
 
 
 
     }
-}
